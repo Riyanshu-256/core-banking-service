57091a2aa12fae56681e72ba4bb3016dc222aefe
Anshu <riyanshu062@gmail.com>
2026-05-26 18:00:48 +0530

    Start Server

---
+++ src/app.js
@@ -0,0 +1,8 @@
+// Import express packages
+const express = require("express");
+
+// Creates backend aap/server
+const app = express();
+
+// export this file, so that another file can used it
+module.exports = app;
