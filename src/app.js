777e97b42f130132108e01035c7e0b8d2ccf8c3a
Anshu <riyanshu062@gmail.com>
2026-05-27 14:01:41 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 
 const authRouter = require("./routes/auth.routes");
+const accountRouter = require("./routes/account.routes");
 
 // Creates backend aap/server
 const app = express();
@@ -12,6 +13,7 @@ app.use(cookieParser());
 
 // All authentication routes are grouped under the /api/auth prefix.
 app.use("/api/auth", authRouter);
+app.use("/api/accounts", accountRouter);
 
 // export this file, so that another file can used it
 module.exports = app;
