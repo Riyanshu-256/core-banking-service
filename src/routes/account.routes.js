97a43138d012762b36603f14ccff6f9e4f7dc96a
Anshu <riyanshu062@gmail.com>
2026-05-28 19:20:42 +0530

    Create account routes

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const authMiddleware = require("../middleware/auth.middleware");
+const accountController = require("../controllers/accountController");
 
 const router = express.Router();
+
+router.post(
+  "/",
+  authMiddleware.authMiddleware,
+  accountController.createAccountController,
+);
 
 module.exports = router;
