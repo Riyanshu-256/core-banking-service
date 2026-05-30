f6b8d074c1b0abfaef373de6d59eaeea736754fc
Anshu <riyanshu062@gmail.com>
2026-05-30 10:54:20 +0530

    Transaction Controller

---
+++ src/controllers/transaction.controller.js
@@ -0,0 +1,81 @@
+const transactionModel = require("../models/transaction.model");
+const ledgerModel = require("../models/ledger.model");
+const accountModel = require("../models/account.model");
+const emailService = require("../services/email.service");
+
+async function createTransaction(req, res) {
+  // 1. Validate request
+  const { fromAccount, toAccount, amount, idempotencyKey } = req.body;
+
+  if (!fromAccount || !toAccount || !amount || !idempotencyKey) {
+    return res.status(400).json({
+      message: "fromAccount, toAccount, amount and idempotencyKey are required",
+    });
+  }
+
+  const fromUserAccount = await accountModel.findOne({
+    _id: fromAccount,
+  });
+
+  const toUserAccount = await accountModel.findOne({
+    _id: toAccount,
+  });
+
+  if (!fromUserAccount || !toUserAccount) {
+    return res.status(400).json({
+      message: "Invalid fromAccount or toAccount",
+    });
+  }
+
+  // 2. Validate idempotencyKey
+  const isTransactionAlreadyExists = await transactionModel.findOne({
+    idempotencyKey: idempotencyKey,
+  });
+
+  if (isTransactionAlreadyExists) {
+    if (isTransactionAlreadyExists.status === "COMPLETED") {
+      return res.status(200).json({
+        message: "Transaction already completed.",
+        transaction: isTransactionAlreadyExists,
+      });
+    }
+
+    if (isTransactionAlreadyExists.status === "PENDING") {
+      return res.status(200).json({
+        message: "Transaction is currently being processed.",
+      });
+    }
+
+    if (isTransactionAlreadyExists.status === "FAILED") {
+      return res.status(200).json({
+        message: "Transaction failed. Please try again.",
+      });
+    }
+
+    if (isTransactionAlreadyExists.status === "REVERSED") {
+      return res.status(200).json({
+        message:
+          "Transaction has been reversed and the amount has been refunded.",
+      });
+    }
+  }
+
+  // 3. Check account status
+  if (
+    fromUserAccount.status !== "ACTIVE" ||
+    toUserAccount.status !== "ACTIVE"
+  ) {
+    return res.status(400).json({
+      message:
+        "Both fromAccount and toAccount must be ACTIVE to process transacction",
+    });
+  }
+
+  // 4. Calculate sender balance from ledger
+  const balance = await fromUserAccount.getBalance();
+  if (balance < amount) {
+    return res.status(400).json({
+      message: `Insufficient balance. Current balance is ${balance}. Requested amount is ${amount}`,
+    });
+  }
+}
